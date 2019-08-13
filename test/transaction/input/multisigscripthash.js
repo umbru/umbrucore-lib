9b090d39578aa7efb968c023dfdbc7a903c54e4b
Ryxor <team@umbru.io>
2019-08-13 16:23:14 +1200

    Initial Commit

---
+++ test/transaction/input/multisigscripthash.js
@@ -0,0 +1,117 @@
+/* eslint-disable */
+// TODO: Remove previous line and work through linting issues at next edit
+
+'use strict';
+/* jshint unused: false */
+
+var should = require('chai').should();
+var expect = require('chai').expect;
+var _ = require('lodash');
+
+var bitcore = require('../../..');
+var Transaction = bitcore.Transaction;
+var PrivateKey = bitcore.PrivateKey;
+var Address = bitcore.Address;
+var Script = bitcore.Script;
+var Signature = bitcore.crypto.Signature;
+var MultiSigScriptHashInput = bitcore.Transaction.Input.MultiSigScriptHash;
+
+describe('MultiSigScriptHashInput', function() {
+
+  var privateKey1 = new PrivateKey('Ky27mcY8BC6JUK6Ab1peZGxSpECy2PQevyj1aEd1uDYY5h1ardo6');
+  var privateKey2 = new PrivateKey('Kz66tJZEf9WVXPEGANB9F89SMjAFvuEFPGQnYf7qXdggspGcF3ZZ');
+  var privateKey3 = new PrivateKey('KxGeK6vWPhRnjV4yQjddzc8Y3CabZHLJ6JMjx7mWQMZeZ2mfTsD8');
+  var public1 = privateKey1.publicKey;
+  var public2 = privateKey2.publicKey;
+  var public3 = privateKey3.publicKey;
+  var address = new Address('3NfRpFy5Sz9g89kduUZqWUKW6nNQtkhfPn');
+
+  var output = {
+    address: '7UiEaDrcNXj1bCQJf1Yc54xkFNr8Ef4sTV',
+    txId: '66e64ef8a3b384164b78453fa8c8194de9a473ba14f89485a0e433699daec140',
+    outputIndex: 0,
+    script: new Script(address),
+    satoshis: 1000000
+  };
+  it('can count missing signatures', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000);
+    var input = transaction.inputs[0];
+
+    input.countSignatures().should.equal(0);
+
+    transaction.sign(privateKey1);
+    input.countSignatures().should.equal(1);
+    input.countMissingSignatures().should.equal(1);
+    input.isFullySigned().should.equal(false);
+
+    transaction.sign(privateKey2);
+    input.countSignatures().should.equal(2);
+    input.countMissingSignatures().should.equal(0);
+    input.isFullySigned().should.equal(true);
+  });
+  it('returns a list of public keys with missing signatures', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000);
+    var input = transaction.inputs[0];
+
+    _.every(input.publicKeysWithoutSignature(), function(publicKeyMissing) {
+      var serialized = publicKeyMissing.toString();
+      return serialized === public1.toString() ||
+              serialized === public2.toString() ||
+              serialized === public3.toString();
+    }).should.equal(true);
+    transaction.sign(privateKey1);
+    _.every(input.publicKeysWithoutSignature(), function(publicKeyMissing) {
+      var serialized = publicKeyMissing.toString();
+      return serialized === public2.toString() ||
+              serialized === public3.toString();
+    }).should.equal(true);
+  });
+  it('can clear all signatures', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000)
+      .sign(privateKey1)
+      .sign(privateKey2);
+
+    var input = transaction.inputs[0];
+    input.isFullySigned().should.equal(true);
+    input.clearSignatures();
+    input.isFullySigned().should.equal(false);
+  });
+  it('can estimate how heavy is the output going to be', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000);
+    var input = transaction.inputs[0];
+    input._estimateSize().should.equal(257);
+  });
+  it('uses SIGHASH_ALL by default', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000);
+    var input = transaction.inputs[0];
+    var sigs = input.getSignatures(transaction, privateKey1, 0);
+    sigs[0].sigtype.should.equal(Signature.SIGHASH_ALL);
+  });
+  it('roundtrips to/from object', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000)
+      .sign(privateKey1);
+    var input = transaction.inputs[0];
+    var roundtrip = new MultiSigScriptHashInput(input.toObject());
+    roundtrip.toObject().should.deep.equal(input.toObject());
+  });
+  it('roundtrips to/from object when not signed', function() {
+    var transaction = new Transaction()
+      .from(output, [public1, public2, public3], 2)
+      .to(address, 1000000);
+    var input = transaction.inputs[0];
+    var roundtrip = new MultiSigScriptHashInput(input.toObject());
+    roundtrip.toObject().should.deep.equal(input.toObject());
+  });
+});
