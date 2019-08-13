e14faccbc4d6951570a651e6506404ea1703b3de
Ryxor <team@umbru.io>
2019-08-13 18:16:47 +1200

    Update x25x

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ bitcore.deps.bs58 = require('bs58');
 bitcore.deps.Buffer = Buffer;
 bitcore.deps.elliptic = require('elliptic');
 bitcore.deps._ = require('lodash');
+bitcore.deps.x25x = require('node-x25x');
 
 // Internal usage, exposed for testing/advanced tweaking
 bitcore.Transaction.sighash = require('./lib/transaction/sighash');
